4409077763cf98c693b4ac05ba037afb916924b1
SorinDEV <42035197+SorinDEV@users.noreply.github.com>
2018-08-02 14:52:19 +0100

    Create romanian.php in lang folder from root

---
+++ lang/romanian.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ *
+ * Romanian (ro) SorinDEV.OVH
+ *
+**/
+
+if (!defined("SorinDEV")) die("This file cannot be accessed directly");
+
+$_LANG['locale'] = "ro_RO";
+
+
+// Meniu //
+$_LANG['home_last_news'] = "Ultimile noutăți";
+$_LANG['home_postedon'] = "Postat în";
+$_LANG['home_view_subject'] = "Vezi subiectul";
+
+// HEADER
+$_LANG['header_home_page'] = "Prima pagină";
+$_LANG['header_home_players_ranking'] = "Clasament jucători";
+$_LANG['header_home_guild_rankings'] = "Clasament bresle";
+$_LANG['header_home_download'] = "Descărcare";
+$_LANG['header_home_register'] = "Înregistrează-te!";
+$_LANG['header_home_logoff'] = "Deconectare";
+
+$_LANG['header_card_title'] = "Metin2 - Acţiunea Orientală MMORPG";
+$_LANG['header_card_desc'] = "In vremuri străvechi răsuflarea Zeului Dragon veghea asupra regatelor Shinsoo, Chunjo şi Jinno. Dar aceasta lume fascinanta a magiei se afla în fata unui pericol imens: Impactul Pietrelor Metin care au cauzat haos şi distrugere pe continent şi intre locuitori. Au izbucnit războaie intre continente, animalele sălbatice s-au transformat în bestii terifiante. Lupta împotriva influentei negative a Pietrelor Metin în postura unui aliat al Zeului Dragon. Aduna-ţi toate puterile şi armele pentru a salva regatul.";
+
+// PAGINA DESCARCARE
+$_LANG['download_title'] = "Descărcare";
+$_LANG['download_direct_button'] = "Descărcare directă";
+$_LANG['download_torrent_button'] = "Descărcare torent";
+$_LANG['download_no_client_alert'] = " Nu există client pentru descărcare. Reveniți mai târziu!";
+$_LANG['download_system_requirements_min'] = "Cerințe minime de sistem";
+$_LANG['download_system_requirements_min_OS'] = "OS";
+$_LANG['download_system_requirements_min_OS_desc'] = "- Win 7, Win 8";
+$_LANG['download_system_requirements_min_CPU'] = "CPU";
+$_LANG['download_system_requirements_min_CPU_desc'] = "- Pentium 4 1.8 GHz";
+$_LANG['download_system_requirements_min_MEMORY'] = "Memorie";
+$_LANG['download_system_requirements_min_MEMORY_desc'] = "- 1 GB";
+$_LANG['download_system_requirements_min_HDD'] = "Hard Disk";
+$_LANG['download_system_requirements_min_HDD_desc'] = "- 3 GB";
+$_LANG['download_system_requirements_min_GR'] = "Placă grafică de memorie";
+$_LANG['download_system_requirements_min_GR_desc'] = "- Placă grafică cu mai mult de 64 MB RAM";
+$_LANG['download_system_requirements_min_SOUND'] = "Placă de sunet";
+$_LANG['download_system_requirements_min_SOUND_desc'] = "- Support DirectX 9.0";
+$_LANG['download_system_requirements_min_MOUSE'] = "Mouse";
+$_LANG['download_system_requirements_min_MOUSE_desc'] = "- Mouse compatibil cu Windows-ul";
+$_LANG['download_system_requirements_rec'] = "Cerințe recomandate de sistem";
+$_LANG['download_system_requirements_rec_OS'] = "OS";
+$_LANG['download_system_requirements_rec_OS_desc'] = "- Win 7, Win 8";
+$_LANG['download_system_requirements_rec_CPU'] = "CPU";
+$_LANG['download_system_requirements_rec_CPU_desc'] = "- Pentium 4 3 GHz";
+$_LANG['download_system_requirements_rec_MEMORY'] = "Memorie";
+$_LANG['download_system_requirements_rec_MEMORY_desc'] = "- 2 GB";
+$_LANG['download_system_requirements_rec_HDD'] = "Hard Disk";
+$_LANG['download_system_requirements_rec_HDD_desc'] = "- 4 GB";
+$_LANG['download_system_requirements_rec_GR'] = "Placă grafică de memorie";
+$_LANG['download_system_requirements_rec_GR_desc'] = "- Placă grafică cu mai mult de 512 MB RAM";
+$_LANG['download_system_requirements_rec_SOUND'] = "Placă de sunet";
+$_LANG['download_system_requirements_rec_SOUND_desc'] = "- Support DirectX 9.0";
+$_LANG['download_system_requirements_rec_MOUSE'] = "Mouse";
+$_LANG['download_system_requirements_rec_MOUSE_desc'] = "- Mouse compatibil cu Windows-ul";
+
+// PAGINA CLASAMENT BRESLE
+$_LANG['guilds_title'] = "Clasament bresle";
+$_LANG['guilds_name'] = "Breaslă";
+$_LANG['guilds_level'] = "Level";
+$_LANG['guilds_wins'] = "Victorii";
+$_LANG['guilds_loss'] = "Înfrângeri";
+$_LANG['guilds_previous_page'] = "Pagina anterioară";
+$_LANG['guilds_next_page'] = "Pagina următoare";
+
+// PAGINA CLASAMENT JUCATORI
+$_LANG['players_title'] = "Clasament Jucători";
+$_LANG['players_name'] = "Jucător";
+$_LANG['players_level'] = "Level";
+$_LANG['players_timep'] = "Timp jucat";
+$_LANG['players_minutes'] = "Minute";
+$_LANG['players_minute'] = "Minut";
+$_LANG['players_previous_page'] = "Pagina anterioară";
+$_LANG['players_next_page'] = "Pagina următoare";
+
+// SIDEBAR DREAPTA
+$_LANG['sidebar_myaccount'] = "Contul meu";
+$_LANG['sidebar_username'] = "Nume de utilizator";
+$_LANG['sidebar_password'] = "Parola";
+$_LANG['sidebar_login'] = "Conectare";
+$_LANG['sidebar_forgot_password'] = "Am uitat parola";
+$_LANG['sidebar_hi'] = "Salut, ";
+$_LANG['sidebar_login_myaccount'] = "Contul meu";
+$_LANG['sidebar_login_changepassword'] = "Schimbă parola";
+$_LANG['sidebar_login_admin'] = "Administrare";
+$_LANG['sidebar_client_download'] = "Descărcare client";
+$_LANG['sidebar_client_latest'] = "Ultima Versiune: ";
+$_LANG['sidebar_client_download_button'] = "Descărcare client";
+$_LANG['sidebar_player_rankings'] = "Clasament Jucători";
+$_LANG['sidebar_player_rankings_player'] = "Jucător";
+$_LANG['sidebar_player_rankings_timep'] = "Timp jucat";
+$_LANG['sidebar_player_rankings_minute'] = "Minut";
+$_LANG['sidebar_player_rankings_minutes'] = "Minute";
+$_LANG['sidebar_player_rankings_fullrank'] = "Clasament Întreg";
+
